1b05de405cd53fec37a58a11b7a95981b6ed7a84
Arthur-Rocha-Lima <2001arthur.rocha@gmail.com>
2021-07-27 21:31:36 -0300

    Adicionado função salvar cliente

--- clientes/src/main/java/br/com/arthurrocha/clientes/rest/ClienteController.java
+++ clientes/src/main/java/br/com/arthurrocha/clientes/rest/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.arthurrocha.clientes.rest;
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.arthurrocha.clientes.model.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
 	private final ClienteRepository repository;
