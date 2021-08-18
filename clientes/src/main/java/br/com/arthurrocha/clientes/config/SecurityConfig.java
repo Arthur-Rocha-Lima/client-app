d2839165f311c5db3787bccf34413f8eaa0714c8
Arthur-Rocha-Lima <2001arthur.rocha@gmail.com>
2021-08-17 21:03:02 -0300

    Usuario e password do banco de dados

--- clientes/src/main/java/br/com/arthurrocha/clientes/config/SecurityConfig.java
+++ clientes/src/main/java/br/com/arthurrocha/clientes/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.arthurrocha.clientes.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,15 +8,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import br.com.arthurrocha.clientes.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{ 
  
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("fulano").password("123").roles("USER");
+		auth
+			.userDetailsService(usuarioService)
+			.passwordEncoder(passwordEncoder());
+			
 	}
 	
 	@Bean
@@ -30,5 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.cors()
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 } 
--- clientes/src/main/java/br/com/arthurrocha/clientes/model/repository/UsuarioRepository.java
+++ clientes/src/main/java/br/com/arthurrocha/clientes/model/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package br.com.arthurrocha.clientes.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.arthurrocha.clientes.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-
+	Optional<Usuario> findByUsername(String username);
 }
